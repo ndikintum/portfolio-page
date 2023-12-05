df3ed777fffd901aaea613b5236b7c3e9b5e958d
ndikintum <ndikintumrodrique2@gmail.com>
2023-12-05 08:24:20 +0100

    correcting linters

--- assets/main.js
+++ assets/main.js
@@ -1,33 +1,33 @@
- // Sample data for projects and technologies
- const projects = [
-  "Project 1: Web App",
-  "Project 2: Mobile App",
-  "Project 3: Data Analysis Tool",
-  "Project 4: E-commerce Website",
-  "Project 5: Chat Application",
-];
+  // Sample data for projects and technologies
+  const projects = [
+  'Project 1: Web App',
+  'Project 2: Mobile App',
+  'Project 3: Data Analysis Tool',
+  'Project 4: E-commerce Website',
+  'Project 5: Chat Application'
+]
 
-let technologies = ["HTML", "CSS", "JavaScript", "React", "Node.js", "Express", "MongoDB"]
+const technologies = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js', 'Express', 'MongoDB']
 
 // Function to update the projects list
 function updateProjectsList () {
-  const projectsList = document.getElementById('projectsList')
-  projectsList.innerHTML = ""
+  const projectsList = document.getElementById('projects-list')
+  projectsList.innerHTML = ''
   projects.forEach(project => {
-      const li = document.createElement('li')
-      li.textContent = project
-      projectsList.appendChild(li)
-  });
+    const li = document.createElement('li')
+    li.textContent = project
+    projectsList.appendChild(li)
+  })
 }
 
 // Function to update the technologies list
 function updateTechList () {
-  const techList = document.getElementById ('techList')
-  techList.innerHTML = ""
+  const techList = document.getElementById('tech-list')
+  techList.innerHTML = ''
   technologies.forEach(tech => {
-      const li = document.createElement('li')
-      li.textContent = tech
-      techList.appendChild(li)
+    const li = document.createElement('li')
+    li.textContent = tech
+    techList.appendChild(li)
   })
 }
 
@@ -40,5 +40,5 @@ updateTechList()
 // updateTechList()
 
 // Example of removing a technology
-// technologies = technologies.filter(tech => tech !== 'Node.js')
+// technologies = technologies.filter(tech => tech !== Node.js)
 // updateTechList()
